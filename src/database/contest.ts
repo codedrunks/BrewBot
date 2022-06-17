51757b6cb7f96167457017df5e03f7f4d44d8c3f
IllusionMan1212 <hisham.abourgheba@gmail.com>
2022-06-18 01:44:53 +0300

    feat: Modal class, most contest cmd functionality
    - implement a Modal class to derive specific modals from.
    - finish most of the contest commands.

--- src/database/contest.ts
+++ src/database/contest.ts
@@ -1,10 +1,42 @@
-import { Contest } from "@prisma/client";
+import { Contest, ContestSubmission } from "@prisma/client";
 import { prisma } from "./client";
 
-export async function setContestChannel(): Promise<void> {
+export async function setContestChannel(guildId: string, channelId: string): Promise<boolean> {
+    await prisma.guild.upsert({
+        where: {
+            id: guildId
+        },
+        update: {
+            contestChannelId: channelId
+        },
+        create: {
+            id: guildId,
+            contestChannelId: channelId
+        }
+    }).catch(() => {
+        return false;
+    });
+
+    return true;
 }
 
-export async function setContestRole(): Promise<void> {
+export async function setContestRole(guildId: string, roleId: string): Promise<boolean> {
+    await prisma.guild.upsert({
+        where: {
+            id: guildId
+        },
+        update: {
+            contestRoleId: roleId
+        },
+        create: {
+            id: guildId,
+            contestRoleId: roleId
+        }
+    }).catch(() => {
+        return false;
+    });
+
+    return true;
 }
 
 export async function getAllContestsInGuild(guildId: string): Promise<Contest[]> {
@@ -18,16 +50,22 @@ export async function getAllContestsInGuild(guildId: string): Promise<Contest[]>
 }
 
 export async function startContest(name: string, description: string, start_date: string, end_date: string): Promise<void> {
+    // TODO:
 }
 
 export async function getCurrentContest(guildId: string): Promise<Contest | null> {
-    const contest = await prisma.contest.findUnique({
+    const contest = await prisma.contest.findFirst({
         where: {
-            guildId_id: {
-                guildId: guildId,
-                // TODO: change this.
-                id: 1,
-            }
+            guildId: guildId,
+            startDate: {
+                lt: new Date(),
+            },
+            endDate: {
+                gt: new Date(),
+            },
+        },
+        orderBy: {
+            id: "desc"
         }
     });
 
@@ -47,12 +85,55 @@ export async function getContestById(guildId: string, contestId: number): Promis
     return contest;
 }
 
-export async function submitContestEntry(contestId: number): Promise<void> {
+export async function getSubmissionsOfContest(guildId: string, contestId: number): Promise<ContestSubmission[]> {
+    const submissions = await prisma.contestSubmission.findMany({
+        where: {
+            guildId: guildId,
+            contestId: contestId
+        },
+    });
+
+    return submissions;
 }
 
-export async function deleteContestSubmission(contestId: number, entryId: string): Promise<void> {
+export async function submitContestEntry(guildId: string, contestId: number, userId: string, submissionLink: string): Promise<boolean> {
+    await prisma.contestSubmission.upsert({
+        where: {
+            guildId_contestId_userId: {
+                guildId: guildId,
+                contestId: contestId,
+                userId: userId
+            },
+        },
+        update: {
+            content: submissionLink,
+        },
+        create: {
+            guildId: guildId,
+            contestId: contestId,
+            userId: userId,
+            content: submissionLink,
+        }
+    }).catch(() => {
+        return false;
+    });
+
+    return true;
 }
 
-export async function startContestVote(contestId: number): Promise<void> {
+export async function deleteContestSubmission(guildId: string, contestId: number, userId: string): Promise<ContestSubmission | null> {
+    const submission = await prisma.contestSubmission.delete({
+        where: {
+            guildId_contestId_userId: {
+                guildId: guildId,
+                contestId: contestId,
+                userId: userId
+            }
+        }
+    }).catch(() => {
+        return null;
+    });
+
+    return submission;
 }
 
