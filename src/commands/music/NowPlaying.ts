4ef5f9799ce4f2c4f565adefdadf818a4503c536
canarado <ajkerik@gmail.com>
2022-06-26 21:43:26 -0400

    better timestring in nowplaying

--- src/commands/music/NowPlaying.ts
+++ src/commands/music/NowPlaying.ts
@@ -1,8 +1,8 @@
 import { CommandInteraction, User } from "discord.js";
 import { Command } from "../../Command";
 import { getManager } from "../../lavalink/client";
-import { embedify } from "../../util";
-import { formatDuration } from "svcorelib";
+import { embedify, musicReadableTimeString } from "../../util";
+import { parseDuration } from "svcorelib";
 
 export class NowPlaying extends Command {
     constructor() {
@@ -25,9 +25,14 @@ export class NowPlaying extends Command {
 
         if(!player || !current) return this.reply(int, embedify("There is no music playing in this server"), true);
 
-        const embed = embedify(`Artist: \`${current?.author}\`\n\n\`${formatDuration(player.position, "%m:%s")}/${formatDuration(current?.duration as number, "%m:%s")}\`\nRequested by: <@${(current?.requester as User).id}>`)
-            .setThumbnail(`https://img.youtube.com/vi/${current?.identifier}/mqdefault.jpg`)
-            .setTitle(`${current?.title}`);
+        const currentTime = parseDuration(player.position);
+        const duration = parseDuration(current?.duration as number);
+
+        const readableTime = musicReadableTimeString(currentTime, duration);
+
+        const embed = embedify(
+            `Artist: \`${current.author}\`\n\n\`${readableTime}\`\nRequested by: <@${(current.requester as User).id}>`
+        ).setThumbnail(`https://img.youtube.com/vi/${current?.identifier}/mqdefault.jpg`).setTitle(`${current?.title}`);
 
         if(current?.uri) embed.setURL(current.uri);
 
--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,5 @@
 import { ColorResolvable, MessageEmbed } from "discord.js";
+import { ParseDurationResult } from "svcorelib";
 import { settings } from "./settings";
 
 const defaultColor = settings.embedColors.default;
@@ -17,4 +18,12 @@ export function formatSeconds(seconds: number): string {
 
 export function nowInSeconds(): number {
     return Math.round(new Date().getTime() / 1000);
+}
+
+export function musicReadableTimeString(position: ParseDurationResult, duration: ParseDurationResult): string {
+    const positionString = `${position.hrs ? (position.hrs > 10 ? `${position.hrs}:` : `0${position.hrs}`) : ""}${position.mins ? (position.mins > 10 ? `${position.mins}:` : `0${position.mins}:`) : "00:"}${position.secs ? (position.secs > 10 ? `${position.secs}` : `0${position.secs}`) : "00"}`;
+    
+    const durationString = `${duration.hrs ? (duration.hrs > 10 ? `${duration.hrs}:` : `0${duration.hrs}`) : ""}${duration.mins ? (duration.mins > 10 ? `${duration.mins}:` : `0${duration.mins}:`) : "00:"}${duration.secs ? (duration.secs > 10 ? `${duration.secs}` : `0${duration.secs}`) : "00"}`;
+
+    return `${positionString}/${durationString}`;
 }
