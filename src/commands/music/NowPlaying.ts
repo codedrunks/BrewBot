0711c0937f6f5da24b6001d4fbf2886917d08d68
canarado <ajkerik@gmail.com>
2022-06-29 16:15:06 -0400

    working nowplaying buttons

--- src/commands/music/NowPlaying.ts
+++ src/commands/music/NowPlaying.ts
@@ -1,8 +1,12 @@
-import { CommandInteraction, User } from "discord.js";
+import { CommandInteraction, GuildMemberRoleManager, MessageButton, User } from "discord.js";
 import { Command } from "@src/Command";
 import { getManager } from "@src/lavalink/client";
-import { embedify, musicReadableTimeString } from "@src/util";
+import { embedify, musicReadableTimeString, TryCatchMethod } from "@src/util";
 import { formatDuration, parseDuration } from "svcorelib";
+import { BtnMsg } from "@src/BtnMsg";
+import { isDJOnlyandhasDJRole } from "@src/database/music";
+
+const ten_secs = 10_000;
 
 export class NowPlaying extends Command {
     constructor() {
@@ -13,10 +17,17 @@ export class NowPlaying extends Command {
         });
     }
 
+    @TryCatchMethod((e, i) => {
+        i?.channel?.send({ embeds: [ embedify("Check console, something went wrong") ]});
+
+        console.error(e);
+    })
     async run(int: CommandInteraction): Promise<void> {
+        await this.deferReply(int);
+
         const guild = int.guild;
 
-        if(!guild) return this.reply(int, embedify("This command cannot be used in DM's")); 
+        if(!guild) return this.editReply(int, embedify("This command cannot be used in DM's")); 
 
         const manager = getManager();
 
@@ -24,7 +35,7 @@ export class NowPlaying extends Command {
 
         const current = player?.queue.current;
 
-        if(!player || !current) return this.reply(int, embedify("There is no music playing in this server"), true);
+        if(!player || !current) return this.editReply(int, embedify("There is no music playing in this server"));
 
         const currentTime = parseDuration(player.position);
         const duration = parseDuration(current?.duration as number);
@@ -37,6 +48,61 @@ export class NowPlaying extends Command {
 
         if(current?.uri) embed.setURL(current.uri);
 
-        return this.reply(int, embed);
+        const btns: MessageButton[] = [
+            new MessageButton().setEmoji("⏪").setLabel("- 10s").setStyle("PRIMARY"),
+            new MessageButton().setEmoji("⏯️").setLabel("Pause/Resume").setStyle("PRIMARY"),
+            new MessageButton().setEmoji("⏩").setLabel("+ 10s").setStyle("PRIMARY"),
+            new MessageButton().setEmoji("⏭").setLabel("Skip").setStyle("PRIMARY"),
+            new MessageButton().setEmoji("⏹️").setLabel("Stop").setStyle("PRIMARY")        ];
+
+        const button = new BtnMsg(embed, btns, { timeout: current.isStream ? -1 : (current.duration as number) - player.position });
+
+        button.on("press", async (b, i) => {
+            await i.deferUpdate();
+
+            if(i.user.id !== int.user.id) return;
+
+            const djCheck = await isDJOnlyandhasDJRole(guild.id, (int.member?.roles as GuildMemberRoleManager).cache);
+
+            console.log("DJCHECK", djCheck);
+
+            if(djCheck) return;
+
+            console.log("HERE2");
+            if(!player || !player.queue.current) return;
+
+            console.log("HERE3");
+
+            switch(b.label) {
+            case "- 10s":
+                player.seek(player.position - ten_secs);
+                break;
+            case "Pause/Resume":
+                player.pause(!player.paused);
+                break;
+            case "+ 10s":
+                player.seek(player.position + ten_secs);
+                break;
+            case "Skip":
+                player.stop();
+                button.emit("timeout");
+                break;
+            case "Stop":
+                player.destroy();
+                button.emit("timeout");
+                break;
+            case "Shuffle":
+                player.queue.shuffle();
+                break;
+            }
+        });
+
+        button.on("timeout", async () => {
+            return button.destroy();
+        });
+
+        await int.editReply({ ...button.getReplyOpts(), embeds: [ embed ]});
+
+        // return this.reply(int, embed);
     }
 }
