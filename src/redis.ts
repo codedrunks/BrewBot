769798b8c061f8ff1cb5776cc27d47ccc5bc9351
canarado <ajkerik@gmail.com>
2022-07-08 16:46:24 -0400

    redis added to economy poggers

--- src/redis.ts
+++ src/redis.ts
@@ -4,7 +4,6 @@ const client = createClient();
 
 client.on("error", (err) => console.log("Redis Client Error", err));
 
-// for some reason, this never gets called, but it should be according to the docs (kinda janky lib tbh)
 client.on("connect", () => {
     console.log("\nRedis Client ready to send commands");
 });
