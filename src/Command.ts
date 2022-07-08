691bca00aec1c73cae3243eb7f1b8745134a9a91
Hesham Abourgheba <hisham.abourgheba@gmail.com>
2022-07-08 16:30:41 +0300

    Sudoku (#36)
    * feat: sudoku game

--- src/Command.ts
+++ src/Command.ts
@@ -219,6 +219,7 @@ export abstract class Command extends EventEmitter
         }
         catch(err)
         {
+            console.error(err);
             const embeds = [ embedify(`Couldn't run the command due to an error${err instanceof Error ? `: ${err.message}` : "."}`, settings.embedColors.error) ];
 
             if(typeof int.reply === "function" && !int.replied && !int.deferred)
