90477b60afc2397f38ef6f8b6dfef8f544f5188d
canarado <ajkerik@gmail.com>
2022-08-03 20:30:16 -0400

    basic voting implementation

--- src/lavalink/lib/trackEnd.ts
+++ src/lavalink/lib/trackEnd.ts
@@ -1,10 +1,13 @@
 import { Client, Collection, GuildMember } from "discord.js";
 import { Player, Track, TrackEndEvent } from "erela.js";
+import { skipVotes } from "@src/commands/music/global.skips";
 
 export function trackEnd(player: Player, track: Track, payload: TrackEndEvent, client: Client) {
     if(!player.voiceChannel) return;
 
     const voiceChannel = client.guilds.cache.get(player.guild)?.channels.cache.get(player.voiceChannel as string)?.members;
+
+    if(skipVotes[player.voiceChannel]) delete skipVotes[player.voiceChannel];
 
     // thinking about wrapping this in a timeout for convenience
     if((voiceChannel as Collection<string, GuildMember>).size <= 1 && player.queue.totalSize > 1) {
