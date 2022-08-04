d94ae446e1da5cb980eb3b0a55f7a0741f35325b
canarado <ajkerik@gmail.com>
2022-08-03 20:56:40 -0400

    cleanup messages

--- src/commands/music/global.skips.ts
+++ src/commands/music/global.skips.ts
@@ -1,10 +1,11 @@
-import { User } from "discord.js";
+import { Message, User } from "discord.js";
 
 interface ISkip {
     amount: number;
     votes: number;
     initiator: User;
-    skippers: Set<string>
+    skippers: Set<string>,
+    lastMessage?: Message<boolean> | undefined,
 }
 
 export const skipVotes: Record<string, ISkip> = {};
