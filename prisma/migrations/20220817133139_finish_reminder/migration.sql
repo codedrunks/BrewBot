b2b976ef1f9cb4b4cd02ba5554a745435c1ec1cf
Sven <s.fehler@pbsnetwork.eu>
2022-08-17 15:33:02 +0200

    fix: revert breaking db migration

--- prisma/migrations/20220817133139_finish_reminder/migration.sql
+++ prisma/migrations/20220817133139_finish_reminder/migration.sql
@@ -2,16 +2,15 @@
   Warnings:
 
   - The primary key for the `Reminder` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `dueTimestamp` on the `Reminder` table. All the data in the column will be lost.
-  - Added the required column `dueTime` to the `Reminder` table without a default value. This is not possible if the table is not empty.
   - Added the required column `reminderId` to the `Reminder` table without a default value. This is not possible if the table is not empty.
+  - Changed the type of `dueTimestamp` on the `Reminder` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
 
 */
 -- AlterTable
 ALTER TABLE "Reminder" DROP CONSTRAINT "Reminder_pkey",
-DROP COLUMN "dueTimestamp",
 ADD COLUMN     "channel" TEXT,
-ADD COLUMN     "dueTime" TIMESTAMP(3) NOT NULL,
 ADD COLUMN     "reminderId" INTEGER NOT NULL,
 ALTER COLUMN "guild" DROP NOT NULL,
+DROP COLUMN "dueTimestamp",
+ADD COLUMN     "dueTimestamp" TIMESTAMP(3) NOT NULL,
 ADD CONSTRAINT "Reminder_pkey" PRIMARY KEY ("reminderId", "userId");
