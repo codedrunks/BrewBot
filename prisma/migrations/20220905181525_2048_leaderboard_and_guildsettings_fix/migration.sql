85d78bc8fc48d0532fc903c0797b9c057656b1a1
Sven Fehler <38158426+Sv443@users.noreply.github.com>
2022-09-07 19:20:08 +0200

    fix prisma deploy (#87)

--- prisma/migrations/20220905181525_2048_leaderboard_and_guildsettings_fix/migration.sql
+++ prisma/migrations/20220905181525_2048_leaderboard_and_guildsettings_fix/migration.sql
@@ -1,8 +1,8 @@
 -- DropForeignKey
-ALTER TABLE "Guild" DROP CONSTRAINT "Guild_id_fkey";
+ALTER TABLE "Guild" DROP CONSTRAINT IF EXISTS "Guild_id_fkey";
 
 -- CreateTable
-CREATE TABLE "TwentyFortyEightLeaderboardEntry" (
+CREATE TABLE IF NOT EXISTS "TwentyFortyEightLeaderboardEntry" (
     "guildId" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "score" INTEGER NOT NULL DEFAULT 0,
@@ -12,7 +12,9 @@ CREATE TABLE "TwentyFortyEightLeaderboardEntry" (
 );
 
 -- AddForeignKey
+ALTER TABLE "GuildSettings" DROP CONSTRAINT IF EXISTS "GuildSettings_guildId_fkey";
 ALTER TABLE "GuildSettings" ADD CONSTRAINT "GuildSettings_guildId_fkey" FOREIGN KEY ("guildId") REFERENCES "Guild"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "TwentyFortyEightLeaderboardEntry" DROP CONSTRAINT IF EXISTS "TwentyFortyEightLeaderboardEntry_guildId_userId_fkey";
 ALTER TABLE "TwentyFortyEightLeaderboardEntry" ADD CONSTRAINT "TwentyFortyEightLeaderboardEntry_guildId_userId_fkey" FOREIGN KEY ("guildId", "userId") REFERENCES "Member"("guildId", "userId") ON DELETE RESTRICT ON UPDATE CASCADE;
