8fbd244286767f57cb31565b53a32fcd73901940
IllusionMan1212 <hisham.abourgheba@gmail.com>
2022-10-14 01:53:31 +0300

    feat: finalize tictactoe game

--- src/commands/games/TicTacToe.ts
+++ src/commands/games/TicTacToe.ts
@@ -1,4 +1,4 @@
-import { APIButtonComponentWithCustomId, ApplicationCommandOptionType, ButtonBuilder, ButtonInteraction, ButtonStyle, CommandInteraction, CommandInteractionOption, EmbedBuilder } from "discord.js";
+import { APIButtonComponentWithCustomId, ApplicationCommandOptionType, ButtonBuilder, ButtonInteraction, ButtonStyle, CommandInteraction, CommandInteractionOption, EmbedBuilder, MessageReaction, User } from "discord.js";
 import { Command } from "@src/Command";
 import { Canvas, CanvasRenderingContext2D, createCanvas } from "canvas";
 import { BtnMsg, embedify } from "@src/utils";
@@ -6,6 +6,7 @@ import fs from "fs-extra";
 import os from "os";
 import path from "path";
 import { settings } from "@src/settings";
+import { randomItem } from "svcorelib";
 
 interface Player {
     id: string;
@@ -20,14 +21,24 @@ interface Game {
     board: Array<Array<Player|null>>;
     canvas: Canvas;
     ctx: CanvasRenderingContext2D;
-    latestInt: CommandInteraction;
     bm: BtnMsg;
 }
 
+interface Move {
+    row: number;
+    idx: number;
+}
+
 const enum Sign {
-    None = "None",
-    X = "X",
-    O = "O"
+    None = "1030120536996593681",
+    X = "1030102333591408660",
+    O = "1030102291644153877"
+}
+
+const enum GameoverState {
+    Win = 0,
+    Tie,
+    None
 }
 
 export class TicTacToe extends Command {
@@ -103,27 +114,42 @@ export class TicTacToe extends Command {
             new EmbedBuilder(),
             [
                 [
-                    new ButtonBuilder().setLabel("\u200b").setStyle(ButtonStyle.Primary),
-                    new ButtonBuilder().setLabel("\u200b").setStyle(ButtonStyle.Primary),
-                    new ButtonBuilder().setLabel("\u200b").setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder().setEmoji("1030120536996593681").setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder().setEmoji("1030120536996593681").setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder().setEmoji("1030120536996593681").setStyle(ButtonStyle.Primary),
                 ],
                 [
-                    new ButtonBuilder().setLabel("\u200b").setStyle(ButtonStyle.Primary),
-                    new ButtonBuilder().setLabel("\u200b").setStyle(ButtonStyle.Primary),
-                    new ButtonBuilder().setLabel("\u200b").setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder().setEmoji("1030120536996593681").setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder().setEmoji("1030120536996593681").setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder().setEmoji("1030120536996593681").setStyle(ButtonStyle.Primary),
                 ],
                 [
-                    new ButtonBuilder().setLabel("\u200b").setStyle(ButtonStyle.Primary),
-                    new ButtonBuilder().setLabel("\u200b").setStyle(ButtonStyle.Primary),
-                    new ButtonBuilder().setLabel("\u200b").setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder().setEmoji("1030120536996593681").setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder().setEmoji("1030120536996593681").setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder().setEmoji("1030120536996593681").setStyle(ButtonStyle.Primary),
                 ],
             ],
             { timeout: 1000 * 60 * 2 },
         );
 
+        bm.on("timeout", async () => {
+            bm.btns.flat().map((btn) => btn.setDisabled(true));
+            int.editReply({
+                ...bm.getReplyOpts(),
+                files: [
+                    {
+                        attachment: path.join(os.tmpdir(), `${user.id}-tictactoe.png`),
+                        name: `${user.id}-tictactoe.png`,
+                        description: `TicTacToe Game for user ${user.id}`,
+                    },
+                ],
+            });
+            await this.cleanUpGame(user.id);
+        });
+
         bm.on("press", async (b, i) => {
             await i.deferUpdate();
-            this.onPress(i, b, game, user.id);
+            this.onPress(i, b, game, user);
         });
 
         const game: Game = {
@@ -146,18 +172,46 @@ export class TicTacToe extends Command {
             },
             canvas: canvas,
             ctx: ctx,
-            latestInt: int,
             bm: bm,
         };
 
         if (humanOpponent) { // le dumb human
-            // TODO:
+            await int.channel?.send({
+                content: `${int.user} has challenged ${humanOpponent} to a game of tic tac toe.`
+            })
+                .then((message) => {
+                    const filter = (_: MessageReaction, user: User) => user.id === humanOpponent.id;
+
+                    message.react("✅");
+                    message.react("❌");
+
+                    message.awaitReactions({filter, max: 1, time: 60000}).then((collected) => {
+                        collected.forEach(async (reaction) => {
+                            message.delete();
+
+                            if(reaction.emoji.name === "✅") {
+                                this.games.set(user.id, game);
+
+                                this.drawBoard(game);
+                                await this.renderBoard(game, user.id);
+                                await this.sendBoard(int, game, user.id);
+                            } else {
+                                return await this.editReply(int, embedify(`${humanOpponent.username} has rejected your challenge`));
+                            }
+                        });
+                    });
+                })
+                .catch(console.error);
         } else { // le smart CPU
             this.games.set(user.id, game);
 
             this.drawBoard(game);
             await this.renderBoard(game, user.id);
             await this.sendBoard(int, game, user.id);
+
+            if (opponentGoesFirst) {
+                this.cpuPlay(game, int, int.user);
+            }
         }
     }
 
@@ -177,12 +231,125 @@ export class TicTacToe extends Command {
     /// Game Logic Functions ////
     /////////////////////////////
 
-    checkWin() {
-        // TODO:
+    getValidMoves(game: Game): Move[] {
+        const moves: Move[] = [];
+
+        game.board.forEach((row, rowNum) => {
+            return row.map((player, idx) => {
+                if (!player) {
+                    moves.push(<Move>{ row: rowNum, idx });
+                }
+            });
+        });
+
+        return moves;
     }
 
-    cpuPlay() {
-        // TODO: make a move and set the currentPlayer back to the initiator (maybe a sleep(2000) so the user can have time to digest)
+    checkGameover(board: (Player | null)[]): GameoverState {
+        const grid = Math.sqrt(board.length);
+
+        // check rows
+        rows:
+        for (let i = 0; i < grid; i++) {
+            for (let j = 0; j < grid; j++) {
+                if (!board[i * grid]) {
+                    continue rows;
+                }
+                if (board[i * grid]?.sign !== board[i * grid + j]?.sign) {
+                    continue rows;
+                }
+            }
+
+            return GameoverState.Win;
+        }
+
+        // check columns
+        cols:
+        for (let i = 0; i < grid; i++) {
+            for (let j = 0; j < grid; j++) {
+                if (!board[i]) {
+                    continue cols;
+                }
+                if (board[i]?.sign !== board[j * grid + i]?.sign) {
+                    continue cols;
+                }
+            }
+
+            return GameoverState.Win;
+        }
+
+        //check diagonals
+        let diagWin = 0;
+        let reverseDiagWin = 0;
+
+        for (let i = 0; i < grid; i++) {
+            if (board[0] && board[i * grid + i] && board[0]?.sign === board[i * grid + i]?.sign) {
+                diagWin++;
+            }
+
+            if (board[grid - 1] && board[i * (grid - 1) + (grid - 1)] && board[grid - 1]?.sign === board[i * (grid - 1) + (grid - 1)]?.sign) {
+                reverseDiagWin++;
+            }
+        }
+
+        if (diagWin === grid || reverseDiagWin === grid) {
+            return GameoverState.Win;
+        }
+
+        if (board.every((player) => player)) {
+            return GameoverState.Tie;
+        }
+
+        return GameoverState.None;
+    }
+
+    cpuPlay(game: Game, int: ButtonInteraction | CommandInteraction, initiator: User) {
+        setTimeout(async () => {
+            const validMoves = this.getValidMoves(game);
+            // TODO: maybe write an algo to actually solve the game instead of random moves
+            const randomMove = randomItem(validMoves);
+            const { row, idx } = randomMove;
+
+            game.bm.btns.flat().map((btn, i) => i === (row * 3 + idx) ? btn.setDisabled(true).setEmoji(game.currentPlayer.sign) : btn);
+            game.board[row][idx] = game.opponent;
+
+            game.currentPlayer = {
+                id: initiator.id,
+                name: initiator.username,
+                icon: initiator.avatarURL() ?? undefined,
+                sign: game.opponent.sign == Sign.X ? Sign.O : Sign.X,
+            };
+
+            const gameover = this.checkGameover(game.board.flat());
+
+            this.drawBoard(game);
+            await this.renderBoard(game, initiator.id);
+
+            switch (gameover) {
+            case GameoverState.Win:
+                (game.bm?.msg as EmbedBuilder[])[0]
+                    .setAuthor({
+                        name: "The CPU won",
+                        iconURL: undefined,
+                    });
+                game.bm.emit("timeout");
+                game.bm.destroy();
+                break;
+            case GameoverState.Tie:
+                (game.bm?.msg as EmbedBuilder[])[0]
+                    .setAuthor({
+                        name: "It's a tie",
+                        iconURL: undefined,
+                    });
+                game.bm.emit("timeout");
+                game.bm.destroy();
+                break;
+            case GameoverState.None:
+            default:
+                await this.nextTurn(game, int, initiator.id);
+                break;
+            }
+        }, 2500);
     }
 
     /////////////////////////
@@ -190,11 +357,11 @@ export class TicTacToe extends Command {
     /////////////////////////
 
     drawX(game: Game, xPos: number, yPos: number) {
-        game.ctx.strokeStyle = "#79bfb6";
+        game.ctx.strokeStyle = "#ef928b";
         game.ctx.lineWidth = 5;
         game.ctx.lineCap = "round";
         game.ctx.lineJoin = "round";
-        game.ctx.shadowColor = "#79bfb6";
+        game.ctx.shadowColor = "#ef928b";
         game.ctx.shadowBlur = 7;
 
         game.ctx.beginPath();
@@ -209,9 +376,9 @@ export class TicTacToe extends Command {
     }
 
     drawO(game: Game, xPos: number, yPos: number) {
-        game.ctx.strokeStyle = "#ef928b";
+        game.ctx.strokeStyle = "#79bfb6";
         game.ctx.lineWidth = 5;
-        game.ctx.shadowColor = "#ef928b";
+        game.ctx.shadowColor = "#79bfb6";
         game.ctx.shadowBlur = 7;
 
         game.ctx.beginPath();
@@ -271,7 +438,7 @@ export class TicTacToe extends Command {
     async sendBoard(int: CommandInteraction, game: Game, userId: string) {
         (game.bm?.msg as EmbedBuilder[])[0]
             .setAuthor({
-                name: `${game.currentPlayer.name}'s (${game.currentPlayer.sign}) turn`,
+                name: `${game.currentPlayer.name}'s (${game.currentPlayer.sign === Sign.X ? "X" : "O"}) turn`,
                 iconURL: game.currentPlayer.icon,
             })
             .setImage(`attachment://${userId}-tictactoe.png`)
@@ -291,7 +458,25 @@ export class TicTacToe extends Command {
     /// Other Functions ////
     ////////////////////////
 
-    async onPress(int: ButtonInteraction, btn: ButtonBuilder, game: Game, userId: string) {
+    async nextTurn(game: Game, int: ButtonInteraction | CommandInteraction, initiatorId: string) {
+        (game.bm?.msg as EmbedBuilder[])[0]
+            .setAuthor({
+                name: `${game.currentPlayer.name}'s (${game.currentPlayer.sign === Sign.X ? "X" : "O"}) turn`,
+                iconURL: game.currentPlayer.icon,
+            });
+        await int.editReply({
+            ...game.bm?.getReplyOpts(),
+            files: [
+                {
+                    attachment: path.join(os.tmpdir(), `${initiatorId}-tictactoe.png`),
+                    name: `${initiatorId}-tictactoe.png`,
+                    description: `TicTacToe Game for user ${initiatorId}`,
+                },
+            ],
+        });
+    }
+
+    async onPress(int: ButtonInteraction, btn: ButtonBuilder, game: Game, initiator: User) {
         if (game.currentPlayer.id !== int.user.id) {
             return;
         }
@@ -300,38 +485,49 @@ export class TicTacToe extends Command {
         const row = Number(btnId[0]);
         const idx = Number(btnId[1]);
 
-        game.bm.btns.flat().map((btn, i) => i === (row * 3 + idx) ? btn.setDisabled(true).setLabel(game.currentPlayer.sign) : btn);
+        game.bm.btns.flat().map((btn, i) => i === (row * 3 + idx) ? btn.setDisabled(true).setEmoji(game.currentPlayer.sign) : btn);
         game.board[row][idx] = game.currentPlayer;
 
         game.currentPlayer = game.currentPlayer.id === game.opponent.id ? {
-            id: int.user.id,
-            name: int.user.username,
-            icon: int.user.avatarURL() ?? undefined,
+            id: initiator.id,
+            name: initiator.username,
+            icon: initiator.avatarURL() ?? undefined,
             sign: game.opponent.sign == Sign.X ? Sign.O : Sign.X,
         } : game.opponent;
 
+        const gameover = this.checkGameover(game.board.flat());
+
         this.drawBoard(game);
-        await this.renderBoard(game, userId);
+        await this.renderBoard(game, initiator.id);
 
-        // TODO: this.checkWin();
+        switch (gameover) {
+        case GameoverState.Win:
+            (game.bm?.msg as EmbedBuilder[])[0]
+                .setAuthor({
+                    name: `${int.user.username} won`,
+                    iconURL: int.user.avatarURL() ?? undefined,
+                }).setColor(settings.embedColors.success);
+            game.bm.emit("timeout");
+            game.bm.destroy();
+            break;
+        case GameoverState.Tie:
+            (game.bm?.msg as EmbedBuilder[])[0]
+                .setAuthor({
+                    name: "It's a tie",
+                    iconURL: undefined,
+                }).setColor(settings.embedColors.gameLost);
+            game.bm.emit("timeout");
+            game.bm.destroy();
+            break;
+        case GameoverState.None:
+        default:
+            await this.nextTurn(game, int, initiator.id);
+            game.opponent.id === "CPU" && this.cpuPlay(game, int, int.user);
+        }
+    }
 
-        (game.bm?.msg as EmbedBuilder[])[0]
-            .setAuthor({
-                name: `${game.currentPlayer.name}'s (${game.currentPlayer.sign}) turn`,
-                iconURL: game.currentPlayer.icon,
-            });
-        await int.editReply({
-            ...game.bm?.getReplyOpts(),
-            files: [
-                {
-                    attachment: path.join(os.tmpdir(), `${userId}-tictactoe.png`),
-                    name: `${userId}-tictactoe.png`,
-                    description: `TicTacToe Game for user ${userId}`,
-                },
-            ],
-        });
-
-        // TODO: check if the opponent is CPU
-        // TODO: this.cpuPlay();
+    async cleanUpGame(userId: string) {
+        this.games.delete(`${userId}`);
+        await fs.rm(path.join(os.tmpdir(), `${userId}-tictactoe.png`));
     }
 }
