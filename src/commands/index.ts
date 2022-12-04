34341bad450231b0f587852ab2ad8fd3262b605f
Sv443 <contact@sv443.net>
2022-12-04 01:25:04 +0100

    fix: remove poll from this branch

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -24,7 +24,6 @@ import { Emoji } from "@commands/util/Emoji";
 import { Exec } from "@commands/util/Exec";
 import { Help } from "@commands/util/Help";
 import { Ping } from "@commands/util/Ping";
-import { Poll } from "@commands/util/Poll";
 import { Reminder } from "@commands/util/Reminder";
 import { Server } from "@commands/util/Server";
 import { Translate } from "@commands/util/Translate";
@@ -88,7 +87,6 @@ export const commands = [
     Exec,
     Help,
     Ping,
-    Poll,
     Reminder,
     Server,
     Translate,
