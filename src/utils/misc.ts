36309cd1fa16aec0f2c5e1d97423b1e255f105d7
Sv443 <contact@sv443.net>
2023-04-10 02:52:27 +0200

    refactor: fix misc texts

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,3 +1,7 @@
+import { Stringifiable } from "svcorelib";
+import k from "kleur";
+import { settings } from "@src/settings";
+
 /** Truncates a string if it exceeds `length` and inserts `endStr` at the end (empty string to disable) */
 export function truncStr(str: string, length: number, endStr = "...")
 {
@@ -25,4 +29,29 @@ export function autoPlural(word: string, num: number | unknown[])
     if(Array.isArray(num))
         num = num.length;
     return `${word}${num === 1 ? "" : "s"}`;
+}
+
+/**
+ * Prints a styled list of items to the console
+ * @param limit Max amount of items per line
+ */
+export function printDbgItmList(list: string[] | Stringifiable[], limit = 10)
+{
+    let msg = "";
+
+    list = list.map(itm => itm.toString()).sort();
+
+    while(list.length > 0)
+    {
+        const items = list.splice(0, limit);
+        msg += `│ ${k.gray(`${items.join(", ")}${items.length === 8 ? "," : ""}`)}\n`;
+    }
+
+    console.log(msg);
+}
+
+/** Triggers the console bell sound if enabled in the .env */
+export function ringBell()
+{
+    settings.debug.bellOnReady && process.stdout.write("\u0007");
 }
